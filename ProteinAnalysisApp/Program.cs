f813309c5015814691bcc82661544d9b6bc45a9f
AnthonyWeidner <alweidner@ucdavis.edu>
2022-08-30 17:37:51 -0700

    Implement business logic for Program.cs

--- ProteinAnalysisApp/Program.cs
+++ ProteinAnalysisApp/Program.cs
@@ -1,5 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-Console.WriteLine("test");
-Console.WriteLine("test2");
+
+using System.Data;
+using ExcelDataReader;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Reading Protein Details");
+
+
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+        FileStream stream = File.Open(@"/Users/AnthonyWeidner/Desktop/SampleExcelFile.xlsx", FileMode.Open, FileAccess.Read);
+        IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+        DataSet result = excelReader.AsDataSet();
+
+        while (excelReader.Read())
+        {
+            int i = 0;
+            Boolean exceptionHasOccurred = false;
+            while (!exceptionHasOccurred)
+            {
+
+                try
+                {
+                    var nextChunk = excelReader.GetString(i);
+                    Console.WriteLine(nextChunk);
+                    ++i;
+                }
+                catch
+                {
+                    exceptionHasOccurred = true;
+                }
+            }
+              
+           
+        };
+
+        
+    }
+
+}
